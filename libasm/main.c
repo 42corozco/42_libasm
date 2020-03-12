a742abcc87ec9e9884436df6768079d320b13d3c
42corozco <corozco>
2020-03-12 06:34:29 +0100

    update README

---
+++ libasm/main.c
@@ -0,0 +1,12 @@
+#include "libasm.h"
+
+#include <stdio.h>
+
+int		main(void)
+{
+	printf("ft_strlen = %d\n", (int)ft_strlen("hola"));
+	printf("ft_strlen = %d\n", (int)ft_strlen(""));
+	printf("ft_strlen = %d\n", (int)ft_strlen("jklansjldsad"));
+
+	return (0);
+}
---
+++ libasm/srcs/ft_strlen.s
@@ -0,0 +1,26 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_strlen.s                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: corozco <corozco@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2020/03/12 06:30:19 by corozco           #+#    #+#              #
+#    Updated: 2020/03/12 06:30:22 by corozco          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+section.text:
+	global _ft_strlen
+
+_ft_strlen:
+	mov rax, 0x0
+	while:
+		cmp BYTE[rdi + rax], 0x0
+		jg cont
+		jmp fin
+	cont:
+		inc rax
+		jmp while
+	fin:
+		ret
